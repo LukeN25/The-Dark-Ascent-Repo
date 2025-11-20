c0c19984e4925414f8a24e8356e36e0f69ea8ec5
HomelessPiano <homelesspiano@gmail.com>
2025-11-20 23:26:33 +0000

    Bunch of Camera Issues

---
+++ Assets/A.Scripts/Gameplay Scripts/CameraManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager Instance;
+
+    [Header("Registered Cameras")]
+    public Camera gameplayCamera;
+    public Camera menuCamera;
+    public Camera logbookCamera;
+    public Camera mutationInventoryCamera;
+
+    private void Awake()
+    {
+        Instance = this;
+        DisableAllCameras();
+        EnableGameplayCamera();
+    }
+
+    public void DisableAllCameras()
+    {
+        if (gameplayCamera) gameplayCamera.enabled = false;
+        if (menuCamera) menuCamera.enabled = false;
+        if (logbookCamera) logbookCamera.enabled = false;
+        if (mutationInventoryCamera) mutationInventoryCamera.enabled = false;
+    }
+
+    public void EnableGameplayCamera()
+    {
+        DisableAllCameras();
+        if (gameplayCamera) gameplayCamera.enabled = true;
+    }
+
+    public void EnableMenuCamera()
+    {
+        DisableAllCameras();
+        if (menuCamera) menuCamera.enabled = true;
+    }
+
+    public void EnableLogbookCamera()
+    {
+        DisableAllCameras();
+        if (logbookCamera) logbookCamera.enabled = true;
+    }
+
+    public void EnableMutationInventoryCamera()
+    {
+        DisableAllCameras();
+        if (mutationInventoryCamera) mutationInventoryCamera.enabled = true;
+    }
+}
